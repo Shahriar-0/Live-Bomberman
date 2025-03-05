5ad8e30d4246784e15a59a04f6b1a28ac6823b46
Shahriar-0 <attarshahriar@gmail.com>
2025-03-05 21:50:23 +0330

    implement TCPManager with server and client setup, including threading and data handling

--- game/src/tcpmanager.cpp
+++ game/src/tcpmanager.cpp
@@ -1,5 +1,91 @@
 #include "tcpmanager.h"
 
-TCPManager::TCPManager(QObject* parent)
-    : NetworkManager{parent} {
+TCPManager::TCPManager(QObject* parent) : NetworkManager(parent),
+                                          m_server(nullptr),
+                                          m_socket(nullptr),
+                                          m_port(0) {
+    connect(m_socket, &QTcpSocket::errorOccurred, this, [this](QAbstractSocket::SocketError error) {
+        emit errorOccurred(m_socket->errorString());
+    });
+    moveToThread(&m_networkThread);
+    m_networkThread.start();
+}
+
+TCPManager::~TCPManager() {
+    stop();
+    m_networkThread.quit();
+    m_networkThread.wait();
+}
+
+void TCPManager::initialize(Role role, const QString& address, quint16 port) {
+    m_role = role;
+    m_address = address;
+    m_port = port;
+
+    QMetaObject::invokeMethod(this, [this]() {
+        if (m_role == Server) setupServer();
+        else setupClient();
+    });
+}
+
+void TCPManager::setupServer() {
+    m_server = new QTcpServer(this);
+    connect(m_server, &QTcpServer::newConnection, this, &TCPManager::onNewConnection);
+
+    if (!m_server->listen(QHostAddress::Any, m_port)) {
+        emit errorOccurred(m_server->errorString());
+    }
+}
+
+void TCPManager::setupClient() {
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, [this]() {
+        emit connectionStatusChanged(true);
+    });
+    connect(m_socket, &QTcpSocket::readyRead, this, &TCPManager::onReadyRead);
+    connect(m_socket, &QTcpSocket::disconnected, this, &TCPManager::onDisconnected);
+
+    m_socket->connectToHost(m_address, m_port);
+}
+
+void TCPManager::onNewConnection() {
+    if (m_role == Server && !m_socket) {
+        m_socket = m_server->nextPendingConnection();
+        connect(m_socket, &QTcpSocket::readyRead, this, &TCPManager::onReadyRead);
+        connect(m_socket, &QTcpSocket::disconnected, this, &TCPManager::onDisconnected);
+        emit connectionStatusChanged(true);
+    }
+}
+
+void TCPManager::onReadyRead() {
+    while (m_socket->bytesAvailable() > 0) {
+        QByteArray data = m_socket->readAll();
+        QJsonDocument doc = QJsonDocument::fromJson(data);
+
+        if (!doc.isNull() && doc.isObject()) {
+            emit dataReceived(doc.object());
+        }
+    }
+}
+
+void TCPManager::onDisconnected() {
+    emit connectionStatusChanged(false);
+    if (m_role == Server) {
+        m_socket->deleteLater();
+        m_socket = nullptr;
+    }
+}
+
+void TCPManager::sendData(const QJsonObject& data) {
+    QMetaObject::invokeMethod(this, [this, data]() {
+        if (m_socket && m_socket->state() == QAbstractSocket::ConnectedState) {
+            QJsonDocument doc(data);
+            m_socket->write(doc.toJson());
+        }
+    });
+}
+
+void TCPManager::stop() {
+    if (m_server) m_server->close();
+    if (m_socket) m_socket->disconnectFromHost();
 }
--- game/src/tcpmanager.h
+++ game/src/tcpmanager.h
@@ -1,11 +1,37 @@
 #ifndef TCPMANAGER_H
 #define TCPMANAGER_H
 
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QThread>
+
 #include "networkmanager.h"
 
 class TCPManager : public NetworkManager {
+    Q_OBJECT
 public:
     explicit TCPManager(QObject* parent = nullptr);
+    ~TCPManager();
+
+    void initialize(Role role, const QString& address, quint16 port) override;
+    void sendData(const QJsonObject& data) override;
+    void stop() override;
+
+private slots:
+    void onNewConnection();
+    void onReadyRead();
+    void onDisconnected();
+
+private:
+    void setupServer();
+    void setupClient();
+
+    QTcpServer* m_server;
+    QTcpSocket* m_socket;
+    QThread m_networkThread;
+    QString m_address;
+    quint16 m_port;
+    Role m_role;
 };
 
 #endif // TCPMANAGER_H
