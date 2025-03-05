1569d53784ceb693565f6ae6a775822ee667d1bd
Shahriar-0 <attarshahriar@gmail.com>
2025-03-05 21:52:02 +0330

    implement UDPManager with threading, data handling, and socket management

--- game/src/udpmanager.cpp
+++ game/src/udpmanager.cpp
@@ -1,5 +1,70 @@
 #include "udpmanager.h"
 
-UDPManager::UDPManager(QObject* parent)
-    : NetworkManager{parent} {
+UDPManager::UDPManager(QObject* parent) : NetworkManager(parent),
+                                          m_socket(nullptr),
+                                          m_port(0) {
+    connect(m_socket, &QUdpSocket::errorOccurred, this, [this](QAbstractSocket::SocketError error) {
+        emit errorOccurred(m_socket->errorString());
+    });
+    moveToThread(&m_networkThread);
+    m_networkThread.start();
+}
+
+UDPManager::~UDPManager() {
+    stop();
+    m_networkThread.quit();
+    m_networkThread.wait();
+}
+
+void UDPManager::initialize(Role role, const QString& address, quint16 port) {
+    m_role = role;
+    m_address = address;
+    m_port = port;
+
+    QMetaObject::invokeMethod(this, [this]() {
+        m_socket = new QUdpSocket(this);
+        connect(m_socket, &QUdpSocket::readyRead, this, &UDPManager::onReadyRead);
+
+        if (m_role == Server) {
+            if (m_socket->bind(QHostAddress::Any, m_port)) {
+                emit connectionStatusChanged(true);
+            }
+            else {
+                emit errorOccurred(m_socket->errorString());
+            }
+        }
+        else {
+            emit connectionStatusChanged(true);
+        }
+    });
+}
+
+void UDPManager::onReadyRead() {
+    while (m_socket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(m_socket->pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+        m_socket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
+
+        QJsonDocument doc = QJsonDocument::fromJson(datagram);
+        if (!doc.isNull() && doc.isObject()) {
+            emit dataReceived(doc.object());
+        }
+    }
+}
+
+void UDPManager::sendData(const QJsonObject& data) {
+    QMetaObject::invokeMethod(this, [this, data]() {
+        QJsonDocument doc(data);
+        QByteArray datagram = doc.toJson();
+
+        QHostAddress address = m_role == Server ? QHostAddress::Broadcast : QHostAddress(m_address);
+        m_socket->writeDatagram(datagram, address, m_port);
+    });
+}
+
+void UDPManager::stop() {
+    if (m_socket) m_socket->close();
 }
--- game/src/udpmanager.h
+++ game/src/udpmanager.h
@@ -1,11 +1,30 @@
 #ifndef UDPMANAGER_H
 #define UDPMANAGER_H
 
+#include <QThread>
+#include <QUdpSocket>
+
 #include "networkmanager.h"
 
 class UDPManager : public NetworkManager {
+    Q_OBJECT
 public:
     explicit UDPManager(QObject* parent = nullptr);
+    ~UDPManager();
+
+    void initialize(Role role, const QString& address, quint16 port) override;
+    void sendData(const QJsonObject& data) override;
+    void stop() override;
+
+private slots:
+    void onReadyRead();
+
+private:
+    QUdpSocket* m_socket;
+    QThread m_networkThread;
+    QString m_address;
+    quint16 m_port;
+    Role m_role;
 };
 
 #endif // UDPMANAGER_H
