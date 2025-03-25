b4a6f1519be1cbfc61126ed614e433feaec838aa
Shahriar-0 <attarshahriar@gmail.com>
2025-03-25 05:09:52 +0330

    remove debug output from keyPressEvent in Player class

--- game/src/player.cpp
+++ game/src/player.cpp
@@ -56,8 +56,8 @@ void Player::keyPressEvent(QKeyEvent* event) {
     }
 
     if (event->key() == Qt::Key_Space) {
-        qDebug() << "fuck u";
-        qDebug() << "Player " << m_playerId << " placed a bomb (inside keyPressEvent).";
+        // qDebug() << "fuck u";
+        // qDebug() << "Player " << m_playerId << " placed a bomb (inside keyPressEvent).";
         placeBomb();
         emit playerPlacedBomb(m_playerId);
         return;
